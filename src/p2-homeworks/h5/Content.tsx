cc85710ee7eb6fdb32bd208b2ab44eeb971c6a50
Den <zgibex333@gmail.com>
2022-02-04 14:05:15 +0300

    hw 5

---
+++ src/p2-homeworks/h5/Content.tsx
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import {Routes, Route, useNavigate, useLocation} from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    // add paths
+}
+
+function Content() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    useEffect(()=> {
+        if(location.pathname === '' || location.pathname === '/') {
+            navigate(PATH.PRE_JUNIOR)
+        }
+    }, [location])
+    return (
+        <div>
+            <Routes>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
+                <Route path="*" element={<Error404/>} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Content
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Content";
+import s from "./HW5.module.css"
+
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [visible, setVisible] = useState(false);
 
+    return (
+        <div  onMouseEnter={() => setVisible(true)}
+              onMouseLeave={() => setVisible(false)}>
+            <ul className={`${s.headerList} ${visible ? s.active: ''}`}>
+                <li><NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink></li>
+                <li><NavLink to={PATH.PRE_JUNIOR}>Junior</NavLink></li>
+                <li><NavLink to={PATH.PRE_JUNIOR}>Junior+</NavLink></li>
+                <li><NavLink to="not-found">NOT FOUND</NavLink></li>
+            </ul>
         </div>
+
+
     )
 }
 
